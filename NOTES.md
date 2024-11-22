db99cb90a06e5bcc5229db1f91a56f2bbc1b7163
Maarten Engels <maartene@thedreamweb.eu>
2024-11-22 12:11:17 +0100

    Findings doing the Kata without TDD

--- NOTES.md
+++ NOTES.md
@@ -8,3 +8,36 @@
 - ⚠️ TODO
 
 ## 🍅 Pomodoro 1
+
+🚧: Implement a car park with multiple levels and multiple stops
+
+Cases:
+✅ On what floor are we?
+✅ In which spot are we?
+✅ How many spots are on a floor? How many steps to take
+✅ Where is the staircase? Left or right?
+✅ Go down the stairs
+✅ Are we on the ground floor?
+✅ Go to the exit
+
+⚠️ What if the start floor is not the latest element in the array?
+⚠️ What if we don't need to go anyway --> Empty array
+
+How did this feel?
+
+- Tiring
+- Fingers crossed
+- Refactor during implementation
+- Unclear what we were working on or trying to solve
+- Feels unsafe
+- Difficult to understand the output, need to debug
+- Difficult to refactor, unclear what breaks
+- We loose oversight of the code, run in to a lot of WTF
+- Unclear what cases we implemented
+- We ended up adding more testcases
+- Naming of the test cases was difficult
+- Difficult to help as navigator
+- Steps too big
+- Way of programming that feels: I don't want to make this my carreer
+- Mental model in your head needs to be way too big
+- Lack of a _shared_ mental model between driver and navigator
--- Tests/CarParkEscapeTests/CarParkEscapeTests.swift
+++ Tests/CarParkEscapeTests/CarParkEscapeTests.swift
@@ -4,3 +4,75 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+func escape(_ carpark: [[Int]]) -> [String] {
+    let carPark = Array(carpark.reversed())
+    
+    // on what floor am I?
+    let startingFloor = carPark.first { floor in
+        floor.contains { space in
+            space == 2
+        }
+    }!
+    
+    let startingFloorIndex = carPark.firstIndex { floor in
+        floor.contains { space in
+            space == 2
+        }
+    }!
+    
+    let startingSpot = startingFloor.firstIndex(of: 2)!
+    
+    var currentFloorIndex = startingFloorIndex
+    var currentSpot = startingSpot
+    var instructions = [String]()
+    
+    while currentFloorIndex != 0 {
+        let currentFloor = carPark[currentFloorIndex]
+        
+        // do we need to go left or right?
+        let stairsSpot = currentFloor.firstIndex(of: 1)!
+        
+        let isLeft = stairsSpot < startingSpot
+        
+        // how many steps
+        let steps = abs(stairsSpot - startingSpot)
+        
+        let instruction = "\(isLeft ? "L" : "R")" + "\(steps)"
+        
+        instructions.append(instruction)
+        currentFloorIndex -= 1
+        currentSpot = stairsSpot
+        instructions.append("D1")
+    }
+    
+    // process the bottom floor
+    let exitSpot = carPark[0].count - 1
+    let steps = abs(currentSpot - exitSpot)
+    instructions.append("R\(steps)")
+    
+    return instructions
+    
+}
+
+@Test("A simple 2 story carpark returns the correct path") func twoStoryCarParkExample() {
+    let carPark = [
+        [1, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0]
+    ]
+    
+    let expectedPath = ["L4", "D1", "R4"]
+    
+    #expect(escape(carPark) == expectedPath)
+}
+
+@Test("For a specific three story carpark, we return the correct path") func threeStoryCarParkExample() {
+    let carpark = [[2, 0, 0, 1, 0],
+                   [0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 0]]
+    
+    let expectedPath = ["R3", "D2", "R1"]
+    
+    #expect(escape(carpark) == expectedPath)
+    
+}
